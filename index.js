1b8412695653bfde5c94b83ecd7e32f55c669f6a
Gautamvaishnav-git <gauravvaishnav8690@gmail.com>
2023-03-27 16:23:06 +0530

    env port added

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const urlRouter = require("./routes/url");
 const staticRouter = require("./routes/static");
 const userRouter = require("./routes/user");
 
-const PORT = 8001;
+const PORT = process.env.PORT | 8001;
 connectToMongo("mongodb://127.0.0.1:27017/shortner");
 
 app.use(express.json());
